0762b65cbdf8ed49c8d6625e7ddad11cf3464b69
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-01-04 08:38:51 +0530

    change name for deployement

--- JAVASCRIPT/script1.js
+++ JAVASCRIPT/script1.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.getElementById("loginForm");
-    const message = document.getElementById("message");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const loginForm = document.getElementById("loginForm");
+//     const message = document.getElementById("message");
 
-    loginForm.addEventListener("submit", function (e) {
-        e.preventDefault();
+//     loginForm.addEventListener("submit", function (e) {
+//         e.preventDefault();
 
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
+//         const username = document.getElementById("username").value;
+//         const password = document.getElementById("password").value;
 
-        // You can implement your authentication logic here.
-        // For a simple example, let's check if the username and password are both "admin".
-        if (username === "ojaswat" && password === "1234567" || username==="palak" && password === "1607" || username==="suraj" && password==="7777777" || username==="vikas" && password==="55555")
-        { 
-            window.location.href = 'home.html';
-        } 
-        else {
-            message.textContent = "Invalid username or password.";
-        }
-    });
-});
+//         // You can implement your authentication logic here.
+//         // For a simple example, let's check if the username and password are both "admin".
+//         if (username === "ojaswat" && password === "1234567" || username==="palak" && password === "1607" || username==="suraj" && password==="7777777" || username==="vikas" && password==="55555")
+//         { 
+//             window.location.href = 'home.html';
+//         } 
+//         else {
+//             message.textContent = "Invalid username or password.";
+//         }
+//     });
+// });
